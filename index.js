b75545f5165450bb399bf63e646a5f7a0789e4f5
NikitaBez <107099081+NikitaBez@users.noreply.github.com>
2022-12-24 13:13:45 +0000

    Changed the logic of adding to the array

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ let d = 10; //Количество сотрудников D
 let salaryD = 100_000; // Зарплата сотрудника D
 let tax = 0.2;
 
-let salary = [a * salaryA, b * salaryB, c * salaryC, d * salaryD];
+let salary = [];
+for (let i = 0; i < a; i++) {
+  salary.push(salaryA);
+}
+for (let i = 0; i < b; i++) {
+  salary.push(salaryB);
+}
+for (let i = 0; i < c; i++) {
+  salary.push(salaryC);
+}
+for (let i = 0; i < d; i++) {
+  salary.push(salaryD);
+}
+
 var salarySum = _.sum(salary);
 let sumTaxes = salarySum * tax;
 console.log("Суммарные налоговые отчисления со всех зарплат: " + sumTaxes + " рублей");
